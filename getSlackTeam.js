1fd55824167e37bea0bad70b82b3aeceeceae91c
danderu <mail@dandel.es>
2017-07-20 13:13:16 +0200

    feat(logs): Adds more logs

--- getSlackTeam.js
+++ getSlackTeam.js
@@ -19,6 +19,8 @@ module.exports = async function getSlackTeam(code) {
 }
 
 function getSlackToken(code) {
+  console.log('SLACK CLIENT ID', config.SLACK_CLIENT_ID)
+  console.log('SLACK CLIENT SECRET', config.SLACK_CLIENT_SECRET)
   return new Promise((resolve, reject) => {
     // request token
     fetch(`https://slack.com/api/oauth.access?code=${code}&client_id=${config.SLACK_CLIENT_ID}&client_secret=${config.SLACK_CLIENT_SECRET}`)
@@ -30,6 +32,7 @@ function getSlackToken(code) {
         return response.json();
       })
       .then(response => {
+        console.log('response token', response)
         resolve(response.access_token)
       })
   })
