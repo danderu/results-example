65cd4afe3e38496a64a7f9f37693df8481c43fa3
Daniel de la Cruz <mail@dandel.es>
2017-08-06 20:54:57 +0200

    feat(micro): set auth with micro

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { send } = require('micro');
-const microAuthSlack = require('microauth-slack');
+const microAuthSlack = require('./micro/microauth-slack');
 
 const options = {
-  clientId: 'CLIENT_ID',
-  clientSecret: 'CLIENT_SECRET',
-  callbackUrl: 'http://localhost:3000/auth/slack/callback',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackUrl: `${process.env.APPLICATION_URL}/auth/slack/callback`,
   path: '/auth/slack',
   scope: 'identity.basic,identity.team,identity.avatar'
 };
@@ -19,13 +19,21 @@ const handler = async (req, res, auth) => {
 
   if (auth.err) {
     // Error handler
-    console.error(auth.err);
     return send(res, 403, 'Forbidden');
   }
 
-  // Save something in database here
-
-  return `Hello ${auth.result.info.user.name}`;
+  return {
+    accessToken: auth.result.accessToken,
+    team: {
+      name: auth.result.info.team.name,
+      domain: auth.result.info.team.domain,
+      logo: auth.result.info.team.image_230
+    },
+    user: {
+      name: auth.result.info.user.name,
+      avatar: auth.result.info.user.image_192
+    }
+  };
 
 };
 
